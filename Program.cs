c4b90b1d3b819e0be8ce65034f93c2c6fd1e95d5
Altug Yildirim <32032244+AltugYildirim@users.noreply.github.com>
2018-11-29 19:44:06 +0300

    Add files via upload
    Autofiltering the desired excel file

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GemBox.Spreadsheet;
+using GemBox.Spreadsheet.ConditionalFormatting;
+using GemBox.Spreadsheet.PivotTables;
+using GemBox.Spreadsheet.Tables;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Windows;
+using Spire.Xls;
+using Spire.Xls.Collections;
+
+namespace AutoFilterExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ActivateAutoFilter(@"C:\temp\Email_Gonderim.xlsx");
+
+            //@"C:\temp\Email_Gonderim.xlsx"
+        }
+        static void ActivateAutoFilter(string path)
+        {
+            Workbook workbook = new Workbook();
+            workbook.LoadFromFile(path);
+            Worksheet sheet = workbook.Worksheets[0];
+            AutoFiltersCollection filters = sheet.AutoFilters;
+            filters.Range = sheet.Range[1, 6, sheet.LastRow, 6];
+            filters.AddFilter(0, "FİLO YÖNETİCİSİ");
+            filters.AddFilter(0, "TPC SORUMLUSU");
+            filters.Filter();
+            workbook.SaveToFile(@"C:\temp\output.xlsx", ExcelVersion.Version2010);
+
+        }
+
+    }
+}
